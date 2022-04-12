3f2021777e684461f95b19e8b14379d571404996
wb_wujianwei <wb_wujianwei@youzan.com>
2022-04-12 11:24:07 +0800

    1

--- src/main/java/com/example/groovydemo/GroovyDemoApplication.java
+++ src/main/java/com/example/groovydemo/GroovyDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GroovyDemoApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(GroovyDemoApplication.class, args);
     }
